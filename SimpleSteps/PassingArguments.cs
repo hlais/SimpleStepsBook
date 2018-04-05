e200131f6d5bd6b3b07b0ca4e3c29d2b3e38ebd5
Halim Lais <halimlais@hotmail.com>
2018-04-05 23:37:30 +0100

    small update

---
+++ SimpleSteps/PassingArguments.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SimpleSteps
+{
+    class PassingArguments
+    {
+        public static void Parameter()
+
+        {
+            Console.Title = "Parameter";
+            double weight;
+            string num;
+        }
+        static string getWeight(out double theWeight)
+        {
+            theWeight = 10;
+            return "Ten";
+        }
+        static double LbToKg(double pounds = 5)
+        {
+            return (pounds * 0.4535);
+        }
+        static void kgToLb(ref double weight)
+        {
+            weight = (weight / 0.4535);
+        }
+    }
+}
